cb8bf72ebe2078166699f252a49bb41cda1fe729
Dragashi <daniel.wehrmaker@gmx.de>
2019-05-04 01:31:15 +0200

    Mybe it works with yellow now

--- BallDetection/ball_detection.py
+++ BallDetection/ball_detection.py
@@ -9,14 +9,18 @@ image = cv2.imread("japan.png")
 # Entfernung: Check
 
 counter=0
+print(image.shape)
+print(image[69,1,1])
+print(image[69,1,0])
 for row in image:
     for column in row:
         blue =column[0]
         red = column[2]
         green = column[1]
-        if(red>blue & red>green):
-            counter += 1
-print(counter)        
+        if(green>blue):
+            if(green>red):
+                counter += 1
+print(counter)       
 cv2.imshow('image',image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
